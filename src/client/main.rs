1019637f3a1e0dc73d24894260dc8a974d3dafbf
imxela <65206220+imxela@users.noreply.github.com>
2024-09-04 18:53:55 +0200

    wip: rewrite

--- src/client/main.rs
+++ src/client/main.rs
@@ -3,6 +3,74 @@
 
 use std::error::Error;
 
+use clap::{Parser, Subcommand};
+
+#[derive(Subcommand, Clone, Debug)]
+pub enum Command {
+    Get,
+    Set { level: i32 },
+    Increase { amount: i32 },
+    Decrease { amount: i32 },
+}
+
+#[derive(Parser)]
+#[command(arg_required_else_help = true)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Option<Command>,
+}
+
+impl Cli {
+    #[must_use]
+    pub fn command(&self) -> Command {
+        self.command.clone().unwrap()
+    }
+
+    fn get() -> Result<(), Box<dyn Error>> {
+        println!("Backlight brightness level is 255");
+        Ok(())
+    }
+
+    fn set(level: i32) -> Result<(), Box<dyn Error>> {
+        println!("Setting backlight brightness level to {}", level);
+        Ok(())
+    }
+}
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let args = Cli::parse();
+
+    match args.command() {
+        Command::Get => {
+            get();
+        }
+        Command::Set { level } => {
+            set(level);
+        }
+        Command::Increase { amount } => {
+            increase(amount);
+        }
+        Command::Decrease { amount } => {
+            decrease(amount);
+        }
+    }
+
     Ok(())
+}
+
+fn get() {
+    println!("0");
+}
+
+fn set(level: i32) {
+    println!("{level}");
+}
+
+fn increase(amount: i32) {
+    println!("{amount}");
+}
+
+fn decrease(amount: i32) {
+    println!("{amount}");
 }
--- src/daemon/main.rs
+++ src/daemon/main.rs
@@ -3,7 +3,7 @@
 
 use std::error::Error;
 
-use config::Config;
+// use config::Config;
 
 pub mod backlight;
 
@@ -12,20 +12,20 @@ use backlight::{Backlight, Sysfs};
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Starting blctl daemon");
 
-    println!("Reading config");
+    // println!("Reading config");
 
-    let config_path = "/etc/blctl/config.toml";
+    // let config_path = "/etc/blctl/config.toml";
 
-    let mut config_dirs = std::path::PathBuf::from(config_path);
-    config_dirs.pop();
-    std::fs::create_dir_all(config_dirs).unwrap();
+    // let mut config_dirs = std::path::PathBuf::from(config_path);
+    // config_dirs.pop();
+    // std::fs::create_dir_all(config_dirs).unwrap();
 
-    let _config = Config::builder()
-        .add_source(config::File::with_name(config_path).required(false))
-        .build()
-        .unwrap();
+    // let _config = Config::builder()
+    //     .add_source(config::File::with_name(config_path).required(false))
+    //     .build()
+    //     .unwrap();
 
-    println!("Initializing backlight");
+    println!("Initializing backlight interface");
 
     let backlight: Box<dyn Backlight> = Box::new(Sysfs::new()?);
 
