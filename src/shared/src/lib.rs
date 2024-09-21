a7b2fee2ebb4dbb5405c7db0a2c1494043923389
imxela <65206220+imxela@users.noreply.github.com>
2024-09-21 14:05:03 +0200

    wip: add documentation to cli

--- src/shared/src/lib.rs
+++ src/shared/src/lib.rs
@@ -2,11 +2,58 @@ use clap::Subcommand;
 
 #[derive(Subcommand, Clone, Debug)]
 pub enum Command {
-    Set { level: i32, raw: bool },
-    Increase { amount: i32, raw: bool },
-    Decrease { amount: i32, raw: bool },
-    Get { raw: bool },
+    /// Set backlight brightness to the specified level
+    Set {
+        /// Desired brightness level between 0 and 100 inclusive
+        level: i32,
+
+        /// Interpret the brightness level to be a raw value defined by the
+        /// backlight device driver as opposed to a value mapped between
+        /// 0 and 100 inclusive
+        #[arg(long)]
+        raw: bool
+    },
+
+    /// Increase backlight brightness by the specified amount
+    Increase {
+        /// Desired brightness level to increase by between 0 and 100 inclusive
+        amount: i32,
+
+        /// Interpret the brightness level to be a raw value defined by the
+        /// backlight device driver as opposed to a value mapped between
+        /// 0 and 100 inclusive
+        #[arg(long)]
+        raw: bool
+    },
+
+    /// Decrease backlight brightness by the specified amount
+    Decrease {
+        /// Desired brightness level to decrease by between 0 and 100 inclusive
+        amount: i32,
+
+        /// Interpret the brightness level to be a raw value defined by the
+        /// backlight device driver as opposed to a value mapped between
+        /// 0 and 100 inclusive
+        #[arg(long)]
+        raw: bool
+    },
+
+    /// Return the current backlight brightness level
+    Get {
+        /// Return the brightness level as a raw value defined by the
+        /// backlight device driver as opposed to a value mapped between
+        /// 0 and 100 inclusive
+        #[arg(long)]
+        raw: bool
+    },
+
+    /// Return the maximum supported backlight brightness level as defined
+    /// by the backlight device driver
     GetMax,
+
+    /// Store the current backlight brightness level on disk
     Store,
+
+    /// Set backlight brightness to the previously stored value
     Restore,
 }
