d6be5431e6631a4e8c3ca4c6ae151368f2cddb40
imxela <65206220+imxela@users.noreply.github.com>
2024-09-28 14:08:49 +0200

    wip: try to fix nix flake

--- flake.nix
+++ flake.nix
@@ -1,19 +1,21 @@
 {
   description = "blctl";
+
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
+
   outputs = { self, nixpkgs }:
-    let
-      supportedSystems = [ "x86_64-linux" ];
-      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      pkgsFor = nixpkgs.legacyPackages;
-    in {
-      packages = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./nix/default.nix { };
-      });
-      devShells = forAllSystems (system: {
-        default = pkgsFor.${system}.callPackage ./nix/shell.nix { };
-      });
-    };
+  let
+    supportedSystems = [ "x86_64-linux" ];
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    pkgsFor = nixpkgs.legacyPackages;
+  in {
+    packages = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./nix/default.nix { };
+    });
+    devShells = forAllSystems (system: {
+      default = pkgsFor.${system}.callPackage ./nix/shell.nix { };
+    });
+  };
 }
--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,21 @@
 {
-  pkgs ? import <nixpkgs> { } }:
-  let
-    manifest = (pkgs.lib.importTOML ../Cargo.toml).package;
-  in
-    pkgs.rustPlatform.buildRustPackage rec {
-      pname = manifest.name;
-      version = manifest.version;
-      cargoLock.lockFile = ../Cargo.lock;
-      src = pkgs.lib.cleanSource ../.;
-    }
+  pkgs ? import <nixpkgs> { } 
+}:
+let
+  manifest = (pkgs.lib.importTOML ../Cargo.toml).package;
+in 
+  pkgs.rustPlatform.buildRustPackage rec {
+    pname = manifest.name;
+    version = manifest.version;
+
+    src = pkgs.lib.fetchFromGitHub {
+      owner = "imxela";
+      repo = "blctl";
+      rev = "";
+      sha256 = pkgs.lib.fakeHash;
+    };
+
+    cargoLock = {
+      lockFile = ../Cargo.lock;
+    };
+  }
