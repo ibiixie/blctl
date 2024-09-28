55a1d1bfcc1e3e79917f9b232e4904df810da5d8
imxela <65206220+imxela@users.noreply.github.com>
2024-09-28 14:55:41 +0200

    wip: bwah5

--- nix/default.nix
+++ nix/default.nix
@@ -12,14 +12,14 @@ in
       owner = "imxela";
       repo = "blctl";
       rev = "d6be5431e6631a4e8c3ca4c6ae151368f2cddb40";
-      sha256 = pkgs.lib.fakeHash;
+      sha256 = "sha256-O9kItZr93sDHWJ8sEy6ANkJQZqnxWTS/1fIveX/2NEE=";
     };
 
     cargoLock = {
       lockFile = ../Cargo.lock;
     };
 
-    cargoDeps = pkgs.rustPlatform.importCargoLock {
-      lockFile = ../Cargo.lock;
-    };
+    # cargoDeps = pkgs.rustPlatform.importCargoLock {
+    #   lockFile = ../Cargo.lock;
+    # };
   }
