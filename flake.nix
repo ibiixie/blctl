2adf1b2092c9e3bd47e7006f3c23d4ff27ad1e12
imxela <65206220+imxela@users.noreply.github.com>
2024-09-28 14:29:27 +0200

    wip: this refuses to work

--- flake.nix
+++ flake.nix
@@ -10,9 +10,12 @@
     supportedSystems = [ "x86_64-linux" ];
     forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     pkgsFor = nixpkgs.legacyPackages;
+
+    blctl = pkgsFor.${system}.callPackage ./nix/default.nix { };
   in {
     packages = forAllSystems (system: {
-      default = pkgsFor.${system}.callPackage ./nix/default.nix { };
+      inherit blctl;
+      default = blctl;;
     });
     devShells = forAllSystems (system: {
       default = pkgsFor.${system}.callPackage ./nix/shell.nix { };
