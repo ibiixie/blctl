6df0bf8b6615f672c75088fb4c8187215a8b2a2a
imxela <65206220+imxela@users.noreply.github.com>
2024-09-29 16:28:54 +0200

    wip fix module again whoopsies

--- nix/module.nix
+++ nix/module.nix
@@ -11,7 +11,7 @@ in {
 
     package = lib.mkOption {
       type = lib.types.package;
-      default = pkgs.callPackage ./package.nix;
+      default = pkgs.callPackage ./package.nix { };
     };
 
     restore = lib.mkOption {
